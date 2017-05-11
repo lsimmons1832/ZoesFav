0a386482543e79f0eff8fe1ecfcaebeb9e156992
Latasha Simmons <lsimmons1832@gmail.com>
2017-05-10 21:18:19 -0500

    save work

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,24 @@
-console.log("I'm linked");
+var app = angular.module("Zoe", []);
+
+app.controller("NavCtrl", ($scope) =>{
+	$scope.zoe = "Zoe's Eats";
+
+});
+
+var spin_count = 1;
+
+$(".skills-wheel .btn").click(function () {
+
+	$('.skills-wheel .wheel').removeClass('spin-' + (spin_count-1));
+
+    $('.skills-wheel .wheel').addClass('spin-' + spin_count);
+    
+    
+    if(spin_count == 1)
+    {
+	    // Popup relevant overlay
+    }
+
+	spin_count++;
+
+});
